f57ca97977e19800fa3179567a98ffeba618c11d
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-25 20:18:35 +0200

    Added segments

---
+++ images.g.ts
@@ -0,0 +1,78 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "T":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 . . 
+. . . 2 2 2 2 2 2 2 . . . 
+`;
+            case "image4":
+            case "M":return img`
+. 2 2 2 2 2 2 2 2 2 . 
+2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 . 
+`;
+            case "image2":
+            case "R":return img`
+. . . 2 
+. . 2 2 
+. 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+. 2 2 2 
+. . 2 2 
+. . . 2 
+`;
+            case "image3":
+            case "L":return img`
+2 . . . 
+2 2 . . 
+2 2 2 . 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 . 
+2 2 . . 
+2 . . . 
+`;
+            case "image5":
+            case "B":return img`
+. . . 2 2 2 2 2 2 2 . . . 
+. . 2 2 2 2 2 2 2 2 2 . . 
+. 2 2 2 2 2 2 2 2 2 2 2 . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
 
+enum SegmentPos {
+    T,
+    TL,
+    TR,
+    M,
+    BL,
+    BR,
+    B
+}
+
+namespace sevenSegment {
+
+    interface Segment {
+        image: Image;
+        x: number;
+        y: number;
+    }
+
+    const segments: Segment[] = [
+        {image: assets.image`T`, x: 0, y: 0}
+    ]
+
+    class SevenSegment extends sprites.ExtendableSprite {
+        segments: boolean[];
+        rendered: Image;
+
+        constructor() {
+            super(image.create(0, 0));
+        }
+
+        toggle(segment: SegmentPos, enabled: boolean) {
+
+        }
+    }
+}
