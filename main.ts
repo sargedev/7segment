c9ed0c18ca62768585e7c07da632bb625b8a8269
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-25 20:47:20 +0200

    Created render method

--- main.ts
+++ main.ts
@@ -19,17 +19,16 @@ namespace sevenSegment {
 
     const segments: Segment[] = [
         {image: assets.image`T`, x: 1, y: 0},
-        {image: assets.image`TL`, x: 0, y: 2},
-        {image: assets.image`TR`, x: 11, y: 2},
+        {image: assets.image`L`, x: 0, y: 2},
+        {image: assets.image`R`, x: 11, y: 2},
         {image: assets.image`M`, x: 2, y: 14},
         {image: assets.image`B`, x: 1, y: 28},
-        {image: assets.image`BL`, x: 0, y: 17},
-        {image: assets.image`BR`, x: 11, y: 17},
+        {image:assets.image`L`, x: 0, y: 17},
+        {image:assets.image`R`, x: 11, y: 17},
     ]
 
     class SevenSegment extends sprites.ExtendableSprite {
-        segments: boolean[];
-        rendered: Image;
+        private segments: boolean[];
 
         constructor() {
             super(image.create(0, 0));
@@ -37,7 +36,23 @@ namespace sevenSegment {
         }
 
         toggle(segment: SegmentPos, enabled: boolean) {
+            this.segments[segment] = enabled;
+            this.render();
+        }
 
+        render() {
+            let x = this.x;
+            let y = this.y;
+            let rendered = image.create(15, 32);
+            let segment: Segment;
+            for (let i = 0; i < 7; i++) {
+                segment = segments[i];
+                if (this.segments[i]) {
+                    rendered.drawTransparentImage(segment.image, segment.x, segment.y);
+                }
+            }
+            this.setImage(rendered);
+            this.setPosition(x, y);
         }
     }
 }
