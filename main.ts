2c2f9de0c10b062baf47433dfc62216e07b238ba
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-25 20:34:49 +0200

    Added all segment objects

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ enum SegmentPos {
     TL,
     TR,
     M,
+    B,
     BL,
-    BR,
-    B
+    BR
 }
 
 namespace sevenSegment {
@@ -18,7 +18,13 @@ namespace sevenSegment {
     }
 
     const segments: Segment[] = [
-        {image: assets.image`T`, x: 0, y: 0}
+        {image: assets.image`T`, x: 1, y: 0},
+        {image: assets.image`TL`, x: 0, y: 2},
+        {image: assets.image`TR`, x: 11, y: 2},
+        {image: assets.image`M`, x: 2, y: 14},
+        {image: assets.image`B`, x: 1, y: 28},
+        {image: assets.image`BL`, x: 0, y: 17},
+        {image: assets.image`BR`, x: 11, y: 17},
     ]
 
     class SevenSegment extends sprites.ExtendableSprite {
@@ -27,6 +33,7 @@ namespace sevenSegment {
 
         constructor() {
             super(image.create(0, 0));
+            this.segments = arrays.repeat(false, 7);
         }
 
         toggle(segment: SegmentPos, enabled: boolean) {
